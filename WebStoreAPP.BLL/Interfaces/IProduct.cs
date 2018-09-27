77f1b6a700c1da05dee98669f88640f1b02af48d
Armin Keco <armin.keco@axis-sg.com>
2018-09-27 03:04:07 +0200

    commit 2

--- WebStoreAPP.BLL/Interfaces/IProduct.cs
+++ WebStoreAPP.BLL/Interfaces/IProduct.cs
@@ -6,5 +6,6 @@ namespace WebStoreAPP.BLL.Interfaces
     public interface IProduct
     {
          IEnumerable<Product> GetAllProducts();
+         //Here need to implement other functionalities
     }
 }
